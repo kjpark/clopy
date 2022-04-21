8ac99bccc5e1b1370cca74e325857a66594437bd
Jedidiah Park <6612147+kjpark@users.noreply.github.com>
2022-04-21 00:30:49 -0400

    implement clap to control program behavior

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,15 @@
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
+
+    let cli = Args::parse();
+    println!("{:?}", cli);
+    // validate & parse later with regex
+    let url = format!("https://api.github.com/repos/{}/tarball/main", cli.source);
+
     let client = reqwest::Client::new();
     let response = client
-        .get("https://api.github.com/repos/kjpark/clopy/tarball/main")
+        .get(url)
         .header(reqwest::header::USER_AGENT, "reqwest")
         .header(reqwest::header::ACCEPT, "application/vnd.github.v3+json")
         .send()
@@ -26,15 +32,39 @@ async fn main() -> Result<(), reqwest::Error> {
         },
     };
 
-    println!("Received {:?} bytes", response.content_length().unwrap());
-    println!("{:?}", response.headers());
+    if cli.verbose {
+        println!("Received {:?} bytes", response.content_length().unwrap());
+        println!("{:?}", response.headers());
+    }
     
+    let destination = match cli.destination {
+        Some(destination) => destination,
+        None => String::from("."),
+    };
+
     let bytes: Vec<u8> = response.bytes().await?.to_vec();
     let tar = flate2::read::GzDecoder::new(&bytes[..]);
     let mut archive = tar::Archive::new(tar);
-    archive.unpack("./output/").unwrap_or_else(|e| {
+    archive.unpack(destination).unwrap_or_else(|e| {
         panic!("Error unpacking tarball: {}", e);
     });
 
     Ok(())
+}
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    // todo owner/repo[:branch|:tag]
+    /// "owner/repo"
+    source: String,
+
+    /// "output/path"
+    destination: Option<String>,
+
+    #[clap(short, long)]
+    /// verbose output?
+    verbose: bool,
 }
